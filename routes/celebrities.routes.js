aa7ab9ef510b7083818cb26a2be2df8a668510f6
claradrojas <drc.clara@gmail.com>
2021-08-03 23:48:54 +0200

    iterations1-4

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,42 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+
+const Celebrity = require("../models/Celebrity.model");
+
+//GET route
+
+router.get('/celebrities/create', (req, res) => res.render("celebrities/new-celebrity"));
+
+/////-----------------------------------
+//POST route
+
+router.post('/celebrities/create', (req, res) => {
+    const { name, occupation, catchPhrase } = req.body;
+
+          Celebrity.create({ name, occupation, catchPhrase })
+
+          .then(dbCelebrities => {
+            res.redirect('/celebrities')
+          })
+
+          .catch(error =>{
+            res.redirect('celebrities/new-celebrity', error)
+            next(error)
+          })
+        
+})
+
+//////-----------
+
+router.get('/celebrities', (res,req, next) => {
+    Celebrity.find() //display all
+    .then(allCelebritiesFromDB => {
+        res.render('celebrities/celebrities',{celebrities: allCelebritiesFromDB} )
+    })
+    .catch(error =>{
+        console.log('There was an error', error)
+    })
+
+})
+
+module.exports = router;
